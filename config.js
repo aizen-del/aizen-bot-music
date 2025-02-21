a867e923a49c9640feee5cb8894b5b45b5362a56
aizen-del <sushant.ayer@icloud.com>
2025-02-21 22:19:06 +0545

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,11 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://aizen:<aizenbot>@musicbot.fyain.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
